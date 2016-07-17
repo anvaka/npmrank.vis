4a5dab28946f79715ff8bc9b46a3dac454732717
anvaka <anvaka@gmail.com>
2016-07-17 10:00:40 -0700

    Fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+rm -rf build
+mkdir build
 npm run build
 cd ./build
+git init
 git add .
 git commit -m 'push to gh-pages'
 git push --force git@github.com:anvaka/npmrank.vis.git master:gh-pages
